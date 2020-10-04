9e2b300c8c56628927743532878c18c835121433
oceaster <oceaster@gmail.com>
2020-10-04 18:24:41 +0100

    new home page design

--- Overlord/Canopy/Templates.py
+++ Overlord/Canopy/Templates.py
@@ -7,21 +7,21 @@ def default_navbar():
     return navbar(
         elements=[
             button(
-                Icon=svg('facebook'),
+                Icon=svg('home'),
                 OnClick=script(
                     'window.open', 
                     'https://www.facebook.com/eastercompany'
                 )
             ),
             button(
-                Icon=svg('twitter'),
+                Icon=svg('user'),
                 OnClick=script(
                     'window.open', 
                     'https://www.twitter.com/eastercompany'
                 )
             ),
             button(
-                Icon=svg('github'),
+                Icon=svg('search'),
                 OnClick=script(
                     'window.open', 
                     'https://github.com/EasterCompany'
@@ -157,20 +157,44 @@ default = [
 
 header = [
     "<div class='site-header'>",
-    "<div class='site-header-logo'>",
-        svg('logo'),
+        "<div class='site-header-logo'>",
+            svg('logo'),
+        """
+        </div>
+        <div style='display:block;'>
+            <h1 style='margin-top:16px;font-family:Caesar Dressing;'> EASTER COMPANY </h1>
+            <br/>
+            <h4> Business Contact </h4>
+            <br/>
+            <h5> owen@easter.company </h5> 
+            <h5> +441382527319 </h5>
+            <br/><hr/> 
+        """,
+        flex(
+            button(
+                Icon=svg('facebook'),
+                OnClick=script(
+                    'window.open', 
+                    'https://www.facebook.com/eastercompany'
+                )
+            ),
+            button(
+                Icon=svg('twitter'),
+                OnClick=script(
+                    'window.open', 
+                    'https://www.twitter.com/eastercompany'
+                )
+            ),
+            button(
+                Icon=svg('github'),
+                OnClick=script(
+                    'window.open', 
+                    'https://github.com/EasterCompany'
+                )
+            )
+        ),
     """
+        </div>
     </div>
-    <div style='display:block;'>
-    <h1 style='margin-top:16px;font-family:Caesar Dressing;'> EASTER COMPANY </h1>
-    <br/>
-    <h4> Business Contact </h4>
-    <br/>
-    <h5> owen@easter.company </h5> 
-    <h5> +441382527319 </h5>
-    <div style='display:flex;margin:32px 0 0 0;justify-content: center;'>
-        <iframe scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:80px; width:80px;" allowTransparency="true" src="https://www.facebook.com/plugins/like.php?locale=en_GB&href=https://www.facebook.com/eastercompany&layout=button&action=like&show_faces=false&share=false&"><div><small><a href="www.fbaddlikebutton.com">http://www.fbaddlikebutton.com</a></small></div><div><small><a href=""></a></small></div></iframe>
-        <a href="https://twitter.com/eastercompany?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @eastercompany</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-    </div></div></div>
     """
 ]
