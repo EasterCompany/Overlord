d891ae2fd7c8efb020a760f705f21aa3b994c596
Owen Easter <owen@easter.company>
2023-10-30 22:19:43 +0000

    updated build script

--- build_shell.sh
+++ build_shell.sh
@@ -1,3 +1,4 @@
+OX_ARGS="$@"
 OX_OS="$1"
 OX_ARCH="$2"
 OX_VERSION="$3"
@@ -8,13 +9,7 @@ OX_SOURCE_DIR="$OX_WORKING_DIR/src"
 OX_BUILD_DIR="$OX_WORKING_DIR/build"
 OX_BUNDLE_DIR="$OX_WORKING_DIR/build/$OX_PACKAGE"
 OX_DEB_FILE="$OX_BUILD_DIR/$OX_PACKAGE.deb"
-OX_ARGS="$@"
-
-if [ $OX_OS == "linux" ]; then
-  OX_BINARY="$OX_BUNDLE_DIR/usr/bin/ox"
-else
-  OX_BINARY="$OX_BUILD_DIR/ox"
-fi
+OX_BINARY="$OX_BUNDLE_DIR/usr/bin/ox"
 
 sudo rm -rf "$OX_BUILD_DIR"
 clear
@@ -37,10 +32,8 @@ if [ "$OX_OS" == "linux" ]; then
   if [ "$OX_ARCH" == "amd64" ]; then
     echo "> Creating new build diretories"
     mkdir -p "$OX_BUILD_DIR"
-    mkdir -p "$OX_BUNDLE_DIR/bin"
     mkdir -p "$OX_BUNDLE_DIR/usr"
     mkdir -p "$OX_BUNDLE_DIR/usr/bin"
-    echo "  Done."
     mkdir -p "$OX_BUNDLE_DIR/DEBIAN"
     echo "  creating package control file..."
     echo "Package: ox
@@ -61,41 +54,27 @@ Description: ox command line interface, package manager, server, and more" > "$O
       exit
     fi
     echo " "
-
-    echo "> Removing Existing Symlinks"
-    if test -f "/usr/bin/ox"; then
-      sudo rm -rf /usr/bin/ox
-      echo "  [DELETED] /usr/bin/ox"
-    fi
-    if test -d "/usr/lib/ox"; then
-      sudo rm -rf /usr/lib/ox
-      echo "  [DELETED] /usr/lib/ox"
-    fi
-    if test -d "/usr/local/lib/ox"; then
-      sudo rm -rf /usr/local/lib/ox
-      echo "  [DELETED] /usr/local/lib/ox"
-    fi
-    echo " "
-
     echo "> Remove Existing Installation"
     sudo apt remove ox -y
     echo " "
-
     echo "> Bundle Debian Package"
     sudo dpkg --build "$OX_BUNDLE_DIR"
     sudo dpkg-deb --info "$OX_DEB_FILE"
     sudo dpkg-deb --contents "$OX_DEB_FILE"
     echo " "
-
     echo "> Install New Package"
     sudo apt-get install -f "$OX_DEB_FILE"
     echo " "
   fi
-  exit
 fi
 
 if [ "$OX_OS" == "darwin" ]; then
   if [ "$OX_ARCH" == "amd64" ]; then
+    echo "> Creating new build diretories"
+    mkdir -p "$OX_BUILD_DIR"
+    mkdir -p "$OX_BUNDLE_DIR/usr"
+    mkdir -p "$OX_BUNDLE_DIR/usr/bin"
+    echo " "
     echo "> Compile binaries from source"
     GOOS="$OX_OS"
     GOARCH="$OX_ARCH"
@@ -108,5 +87,18 @@ if [ "$OX_OS" == "darwin" ]; then
       exit
     fi
   fi
-  exit
+  if test -f "/usr/bin/ox"; then
+    echo "> Removing Symlinks"
+    sudo rm -rf /usr/bin/ox
+    echo "  [DELETED] /usr/bin/ox"
+    echo " "
+  fi
+  echo "> Generate Symlinks"
+  sudo ln -s $OX_BUNDLE_DIR/usr/bin/ox /usr/bin/ox
+  if test -f "/usr/bin/ox"; then
+    echo "  [GENERATED] /usr/bin/ox"
+  fi
+  echo " "
 fi
+
+exit
