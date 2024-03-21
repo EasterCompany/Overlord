afc667633fcdb6fa9a1170f13427333335ea1306
Owen Easter <owen@easter.company>
2024-03-21 22:14:10 +0000

    integrated sh module

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/EasterCompany/Overlord
 
 go 1.22.1
 
-require github.com/EasterCompany/sh v1.1.0 // direct
+require github.com/EasterCompany/sh v1.2.2 // direct
--- main.go
+++ main.go
@@ -1,10 +1,53 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"strings"
+
 	"github.com/EasterCompany/sh"
 )
 
+var (
+	DEBUG      bool   = false
+	EXECUTABLE string = sh.ParseExecutable()
+)
+
 func main() {
-	sh.Exec("echo", "hello")
-	// fmt.Print(resp)
+	if strings.HasPrefix(EXECUTABLE, "/tmp/go-build") {
+		DEBUG = true
+	}
+	if len(os.Args) >= 2 {
+		executeCmdLine()
+	} else {
+		enterFocusMode()
+	}
 }
+
+func enterFocusMode() {
+	sh.Run("clear")
+	displayBanner()
+	for {
+		awaitInput()
+	}
+}
+
+func executeCmdLine() {
+	// Parse user input from the command line
+	target := sh.ParseTarget()
+	globals := sh.ParseGlobals()
+	flags := sh.ParseFlags()
+	options := sh.ParseOptions()
+	// Log user input when in debug mode
+	if DEBUG {
+		fmt.Println("TARGET:", target)
+		fmt.Println("GLOBALS:", globals)
+		fmt.Println("FLAGS:", flags)
+		fmt.Println("OPTIONS:", options)
+		fmt.Println("FULL:", os.Args)
+	}
+}
+
+func displayBanner() {}
+
+func awaitInput() {}
