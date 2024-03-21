f0f81daa2bbdcba3bc5021b2e14b5ecc63fbb781
Owen Easter <owen@easter.company>
2024-03-21 15:46:05 +0000

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/EasterCompany/Overlord
+
+go 1.22.1
+
+require github.com/EasterCompany/sh v1.1.0 // direct
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/EasterCompany/sh"
+)
+
+func main() {
+	sh.Exec("echo", "hello")
+	// fmt.Print(resp)
+}
